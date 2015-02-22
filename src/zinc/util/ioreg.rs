3022539ea9fef56ba3e2b0c07850b9a5cb4234d0
Allen Welkie <allen@localhost.localdomain>
2015-02-22 18:04:36 -0500

    Ensure layout of register structs

--- src/zinc/util/ioreg.rs
+++ src/zinc/util/ioreg.rs
@@ -17,6 +17,7 @@ macro_rules! ioreg_old(
   ($io:ident: $ty:ty, $($reg:ident),+) => (
     #[allow(non_snake_case)]
     #[derive(Copy)]
+    #[repr(C, packed)]
     pub struct $io {
       $(
         $reg: VolatileCell<$ty>,
